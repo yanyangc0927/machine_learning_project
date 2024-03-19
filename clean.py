4cd5e681f266fd3cd3acac26dfb2ef70bc394f1d
yanyangc0927 <162510599+yanyangc0927@users.noreply.github.com>
2024-03-18 23:11:49 -0400

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,7 +1,17 @@
 import pandas as pd
 
+
 def clean(input_file):
-    df=pd.read_csv(input_file)
-    
+    df = pd.read_csv(input_file)
     return df
 
+
+if __name__ == '__main__':
+    import argparse
+
+    parse = argparse.ArgumentParser()
+    parse.add_argument('input', help='diabetes file')
+    parse.add_argument('output', help='cleaned diabetes file')
+    args = parse.parse_args()
+    cleaned = clean(args.input)
+    cleaned.to_csv(args)
